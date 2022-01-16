5a5e50090fbf1432e031a437d2064836401b9d12
DarshanVaishya <DarshanVaishya3@gmail.com>
2022-01-16 19:23:47 +0530

    Refactored the code

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 "use strict";
 
+// Globals
+const checkBtn = document.querySelector(".btn.check");
+const againBtn = document.querySelector(".btn.again");
+const resetBtn = document.querySelector(".btn.reset");
+
+const highscoreEl = document.querySelector(".highscore");
+const messageEl = document.querySelector(".message");
+const scoreEl = document.querySelector(".score");
+const titleEl = document.querySelector(".title");
+const guessEl = document.querySelector(".guess");
+const numberEls = document.querySelectorAll(".number");
+
+let secretNumber = getRandomNumber(1, 21);
+let score = 20;
+
+// Functions
 function getRandomNumber(min, max) {
 	const number = Math.floor(Math.random() * (max - min) + min);
 	console.log(number);
@@ -7,80 +23,71 @@ function getRandomNumber(min, max) {
 }
 
 function displayMessage(message) {
-	document.querySelector(".message").textContent = message;
+	messageEl.textContent = message;
 }
 
-let secretNumber = getRandomNumber(1, 21);
-const scoreEl = document.querySelector(".score");
-let score = 20;
+// Setting highscore
+highscoreEl.textContent = localStorage.getItem("highscore") || 0;
 
-if (localStorage.getItem("highscore")) {
-	document.querySelector(".highscore").textContent =
-		localStorage.getItem("highscore");
-}
-
-document.querySelector(".check").addEventListener("click", () => {
-	if (score <= 1) {
+checkBtn.addEventListener("click", () => {
+	if (score < 1) {
 		displayMessage("Game over");
-		document.querySelector("body").style.backgroundColor = "red";
+		document.body.style.backgroundColor = "red";
 		scoreEl.textContent = "0";
 
-		document.querySelector(".check").style.display = "none";
-		document.querySelector(".again").style.display = "block";
-		document.querySelector(".title").textContent = "Game over";
+		checkBtn.style.display = "none";
+		againBtn.style.display = "block";
+		titleEl.textContent = "Game over";
 		return;
 	}
 
-	const guess = +document.querySelector(".guess").value;
+	const guess = +guessEl.value;
 	if (!guess) {
 		displayMessage("No number!");
 	} else if (guess < 1 || guess > 20) {
 		displayMessage("Must be between 1 and 20");
 	} else if (guess === secretNumber) {
 		displayMessage("Correct number");
-		document.querySelector("body").style.backgroundColor = "#60b347";
+		document.body.style.backgroundColor = "#60b347";
 
 		// Replacing with correct number
-		let numberEls = document.getElementsByClassName("number");
-		for (let i = 0; i < numberEls.length; i++) {
-			numberEls[i].textContent = secretNumber;
-		}
+		numberEls.forEach((element) => (element.textContent = secretNumber));
 
 		// Updating and storing the highscore
-		const highscore = +document.querySelector(".highscore").textContent;
+		const highscore = +highscoreEl.textContent;
 		if (score > highscore) {
-			document.querySelector(".highscore").textContent = score;
+			highscoreEl.textContent = score;
 			localStorage.setItem("highscore", score);
 		}
 
-		document.querySelector(".check").style.display = "none";
-		document.querySelector(".again").style.display = "block";
-		document.querySelector(".title").textContent = "Correct number";
+		checkBtn.style.display = "none";
+		againBtn.style.display = "block";
+		titleEl.textContent = "Correct number";
 	} else {
 		displayMessage(guess > secretNumber ? "Too high" : "Too low");
 		scoreEl.textContent = --score;
 	}
 });
 
-document.querySelector(".btn.again").addEventListener("click", () => {
+againBtn.addEventListener("click", () => {
 	score = 20;
 	secretNumber = getRandomNumber(1, 21);
 
 	displayMessage("Start guessing...");
 	scoreEl.textContent = score;
-	document.querySelector("body").style.backgroundColor = "#222";
-	document.querySelector(".guess").value = "";
+	document.body.style.backgroundColor = "#222";
+	guessEl.value = "";
 	let numberEls = document.getElementsByClassName("number");
 	for (let i = 0; i < numberEls.length; i++) {
 		numberEls[i].textContent = "?";
 	}
 
-	document.querySelector(".again").style.display = "none";
-	document.querySelector(".check").style.display = "block";
-	document.querySelector(".title").textContent = "Guess the number";
+	againBtn.style.display = "none";
+	checkBtn.style.display = "block";
+	titleEl.textContent = "Guess the number";
 });
 
-document.querySelector(".btn.reset").addEventListener("click", () => {
+resetBtn.addEventListener("click", () => {
 	localStorage.removeItem("highscore");
-	document.querySelector(".highscore").textContent = "0";
+	highscoreEl.textContent = "0";
 });
